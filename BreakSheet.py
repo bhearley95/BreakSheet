15b72d797065e985068b7549b47c28c10027dadf
bhearley95 <bhearley95@gmail.com>
2024-05-12 12:12:03 -0400

    Update BreakSheet.py

--- BreakSheet.py
+++ BreakSheet.py
@@ -16,19 +16,38 @@ home = '/mount/src/breaksheet'
 prod_dir = '/mount/src/breaksheet/ProductSheets'
 os.chdir(prod_dir)
 files_all = glob.glob('*.xlsx')
-prod_all = []
+prod_dict = {}
 for i in range(len(files_all)):
   name = files_all[i][:len(files_all[i])-5]
   data = name.split(' ')
   year = data[0]
   brand = data[1]
   product = name[len(year) + len(brand) + 2:]
-  prod_all.append([year, brand, product])
+  # Set Up Dictionary
+  year_keys = list(prod_dict.keys())
+  if year not in year_keys:
+    prod_dict[year] = {}
+  brand_keys = list(prod_dict[year].keys())
+  if brand not in brand_keys:
+    prod_dict[year][brand] = []
+  prod_keys = prod_dict[year][brand]
+  if product not in prod_keys:
+    prod_dict[year][brand].append(product)
 os.chdir(home)
 
 # Get the products
-grid_prod = st.columns([0.4,0.6])
-with grid_prod[0]:
+grid_num_prod = st.columns([0.4,0.6])
+with grid_num_prod[0]:
   num_prod = st.number_input('Total number of products in the break',  min_value=0, max_value=None, value=0, step=1)
 
+grid_prod = st.columns([0.2,0.2,0.4,0.2])
+year_choice = []
+def add_row_prod(row):
+  with grid_prod[0]:
+    while len(year_choice) < row+1:
+      year_choice.append(None)
+    if row == 0:
+      year_choice[row] = st.select_box('Year',(list(prod_dict.keys())), key = f'input_col_yr{row}')
+    else:
+      year_choice[row] = st.select_box('Year',(list(prod_dict.keys())), key = f'input_col_yr{row}', label_visibility = "collpased")
 
