cecb4a2e87a6b43afababe5ab597b03a7a258e53
bhearley95 <bhearley95@gmail.com>
2024-05-12 11:50:15 -0400

    Update BreakSheet.py

--- BreakSheet.py
+++ BreakSheet.py
@@ -10,4 +10,6 @@ st.title('NBA Team Breaks Sheets')
 st.markdown('Select the products in your break to view the full checklist.')
 
 # Get the products
-num_prod = st.number_input('Total number of products in the break',  min_value=0, max_value=None, value=0, step=1)
+grid_prod = st.columns([0.6,0.4])
+with grid_prod[0]:
+  num_prod = st.number_input('Total number of products in the break',  min_value=0, max_value=None, value=0, step=1)
