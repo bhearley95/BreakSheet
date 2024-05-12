055e4d5c4bd2e7034ee4da659217fafa94a8a530
bhearley95 <bhearley95@gmail.com>
2024-05-12 11:46:06 -0400

    Update BreakSheet.py

--- BreakSheet.py
+++ BreakSheet.py
@@ -2,4 +2,10 @@
 import streamlit as st
 import pandas as pd
 
+# Set Page Configuration
+st.set_page_config(layout="wide")
+
+# Set Title
+st.title('NBA Team Breaks Sheets')
+st.markdown('Select the products in your break to view the full checklist.')
 
